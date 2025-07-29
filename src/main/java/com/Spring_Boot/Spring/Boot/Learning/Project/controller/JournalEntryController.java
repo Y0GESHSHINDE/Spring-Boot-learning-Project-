af97bcc227ec01a2d1b8bf2dd82389d18fccdfa6
Yogesh Shinde <yogeshshinde3624@gmail.com>
2025-07-29 07:09:56 +0530

    journal app

---
+++ src/main/java/com/Spring_Boot/Spring/Boot/Learning/Project/controller/JournalEntryController.java
@@ -0,0 +1,12 @@
+package com.Spring_Boot.Spring.Boot.Learning.Project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class JournalEntryController {
+
+
+}
